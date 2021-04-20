5609cc560cd8bd1fd06baf10309f7061561577fe
Vince <vince1997mu@gmail.com>
2021-04-21 00:08:58 +1000

    fix: miniconda installation

---
+++ install_miniconda.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+!(stat -t /usr/local/lib/*/dist-packages/google/colab > /dev/null 2>&1) && exit 
+
+# change into working directory on colab
+cd /content/
+
+## install miniconda
+curl -sL \
+  "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > \
+  "Miniconda3.sh"
+
+chmod +x ./Miniconda3.sh
+./Miniconda3.sh -b
+
+exec bash -l
+
+echo "Miniconda installed"
--- setup_collab.sh
+++ setup_collab.sh
@@ -6,14 +6,8 @@
 cd /content/
 
 ## install miniconda
-curl -sL \
-  "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" > \
-  "Miniconda3.sh"
-
-chmod +x ./Miniconda3.sh
-./Miniconda3.sh -b
-
-exec bash -l
+chmod +x ./install_miniconda.sh
+./install_miniconda.sh
 
 # setup python environment
 conda create -n semantic_segmentation python=3.5 -y
