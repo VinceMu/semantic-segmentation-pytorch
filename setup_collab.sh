0e1b11770e30f933b7993be7b3973160fed8bfc5
Vince <vince1997mu@gmail.com>
2021-04-22 00:06:26 +1000

    chore: added logging to setup scripts

--- setup_collab.sh
+++ setup_collab.sh
@@ -11,12 +11,15 @@ conda install pip -y
 conda install pytorch=0.4.1 cuda90 -c pytorch
 conda install -c pytorch torchvision
 pip install -r requirements.txt
-source activate semantic_segmentation
+conda activate semantic_segmentation
+echo "python environment setup"
 
 # download ADE20K dataset
 chmod +x ./download_ADE20K.sh
 ./download_ADE20K.sh
+echo "ADE20K dataset downloaded"
 
 # remask dataset for training and validation
 python ./remask_dataset/parse_training_set.py ./data/ADEChallengeData2016/annotations/training ./data/ADEChallengeData2016_remasked/annotations/training
 python ./remask_dataset/parse_training_set.py ./data/ADEChallengeData2016/annotations/validation ./data/ADEChallengeData2016_remasked/annotations/validation
+echo "dataset remasked"
